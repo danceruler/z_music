d83e4a7b1d3c45bff307f11caf60c338e34c3eee
Dance ruler <oncwnuEWrTZH7ZJqbqIYQkIlJ1pU@git.weixin.qq.com>
2020-04-13 18:07:14 +0800

    lyric_SolveKugouInSomeNetNoUseProblem

--- z_music/lib/Model/Api.dart
+++ z_music/lib/Model/Api.dart
@@ -5,6 +5,10 @@ import 'package:cookie_jar/cookie_jar.dart';
 class ApiList{
   static const kugou_searchList = "https://songsearch.kugou.com/song_search_v2?callback={4}&keyword={0}&page={1}&pagesize={2}&userid=-1&clientver=&platform=WebFilter&tag=em&filter=2&iscorrection=1&privilege_filter=0&_={3}";
   static const kugou_getMusicInfo = "https://wwwapi.kugou.com/yy/index.php?r=play/getdata&callback={0}&hash={1}&album_id={2}&dfid=1aJS0t0U5yRr0qQNef02gPgO&mid=a1d421b9093150b003fbcd4de3ac3b15&platid=4&_={3}";
+  static const kugou_mobile_musicinfo = "https://m3ws.kugou.com/api/v1/song/get_song_info?cmd=playInfo&hash={0}&from=mkugou&apiver=2&mid=193ec0b068520872b2803af3a5dd7515&userid=0&platid=4&dfid=1iARnm4XWB5b0rPlMz3n45tj";
+  static const kugou_mobile_lyric = "https://m3ws.kugou.com/app/i/krc.php?cmd=100&keyword=&hash={0}&timelength=141000&d=0.46360261656668444";
+  static const kugou_mobile_img = "https://mtools.kugou.com/api/v1/singer_header/get_by_hash?hash={0}&size=200&format=jsonp&callback={1}}";
+
   static const qq_searchlist = "https://c.y.qq.com/soso/fcgi-bin/client_search_cp?ct=24&qqmusic_ver=1298&new_json=1&remoteplace=txt.yqq.center&searchid=46898848196070831&t=0&aggr=1&cr=1&catZhida=1&lossless=0&flag_qc=0&p={0}&n={1}&w={2}&g_tk_new_20200303=126304154&g_tk=126304154&jsonpCallback={3}&loginUin={4}&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0";
   static const qq_getMusicInfo = "https://u.y.qq.com/cgi-bin/musicu.fcg?callback={0}&g_tk=1&jsonpCallback={0}&loginUin={1}&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&data={2}";
   static const qq_coverImg = "https://y.gtimg.cn/music/photo_new/T002R300x300M000{0}.jpg?max_age=2592000";
--- z_music/lib/util/music/kugouMusic.dart
+++ z_music/lib/util/music/kugouMusic.dart
@@ -16,12 +16,15 @@ import 'BasicMusic.dart';
 //不要短时间内多次调用getMusicInfo，
 class KugouMusic implements BasicMusic {
   static String requestCallback = "";
+  static String requestMobileImgCallback = "";
   static String timeStamp = "";
 
   KugouMusic() {
     if (timeStamp == "") timeStamp = Util.currentTimeMillis().toString();
     if (requestCallback == "")
       requestCallback = "jQuery112409513165674145783_" + timeStamp;
+    if (requestMobileImgCallback == "")
+      requestMobileImgCallback = "kgJSONP179900027";
   }
 
   @override
@@ -34,7 +37,7 @@ class KugouMusic implements BasicMusic {
       timeStamp,
       requestCallback
     ]);
-    
+
     await http.get(apiUrl).then((response) {
       RegExp reg = new RegExp(r"(?=" + requestCallback + "\()(.*)(?=\))");
       var jsonStr = Util.onlyMatchOne(reg, response.body);
@@ -58,9 +61,8 @@ class KugouMusic implements BasicMusic {
               mvHash: item.MvHash);
           result.add(music);
         }
-        
       } else {
-        throw ReqException("请求异常，请尝试打开网页版酷狗音乐解锁");
+        throw ReqException("请求异常，请确定该网络是否可以正常使用酷狗音乐网页版播放歌曲");
       }
     });
     await getMusicInfo(result[0]);
@@ -69,50 +71,119 @@ class KugouMusic implements BasicMusic {
 
   @override
   Future<Music> getMusicInfo(Music music) async {
-    String apiUrl = Util.stringFormat(ApiList.kugou_getMusicInfo,
-        [requestCallback, music.fileHash, music.albumId, timeStamp]);
-    Map<String,String> headers = {
-      "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-      "Accept-Encoding":"gzip, deflate, br",
-      "Accept-Language":"zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
-      "Cache-Control":"max-age=0",
-      "Connection":"keep-alive",
-      "Cookie":"kg_mid=a1d421b9093150b003fbcd4de3ac3b15; Hm_lvt_aedee6983d4cfc62f509129360d6bb3d=1586421377,1586439179,1586509537,1586510990; kg_dfid=1aJS0t0U5yRr0qQNef02gPgO; Hm_lpvt_aedee6983d4cfc62f509129360d6bb3d=1586511002; kg_dfid_collect=d41d8cd98f00b204e9800998ecf8427e",
-      "Host":"wwwapi.kugou.com",
-      "If-Modified-Since":"Fri, 10 Apr 2020 09:55:57 GMT",
-      "TE":"Trailers",
-      "Upgrade-Insecure-Requests":"1",
-      "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0"
-    };
-    // List<Cookie> cookies = [
-    //   new Cookie("kg_mid", "a1d421b9093150b003fbcd4de3ac3b15"),
-    //   new Cookie("kg_mid_temp", "a1d421b9093150b003fbcd4de3ac3b15"),
-    //   new Cookie("kg_dfid_collect", "d41d8cd98f00b204e9800998ecf8427e"),
-    //   new Cookie("kg_dfid", "1aJS0t0U5yRr0qQNef02gPgO"),
-    // ];
-    // Api.cookieJar.saveFromResponse(Uri.parse(apiUrl), cookies);
-    // Dio dio = Dio();
-    // dio.interceptors.add(CookieManager(await Api.cookieJar));
-    // var test = await dio.get(apiUrl);
-    // print(test);
-    await http.get(apiUrl,headers: headers).then((response) {
-      RegExp reg = new RegExp(r"(?=" + requestCallback + "\()(.*)(?=\))");
-      var jsonStr = Util.onlyMatchOne(reg, response.body);
-      jsonStr = jsonStr.substring(1, jsonStr.length - 2);
-      var jsonMap = jsonDecode(jsonStr);
-      if (jsonMap["err_code"] == 0) {
-        music.coverUrl = jsonMap["data"]["img"];
-        music.playUrl = jsonMap["data"]["play_url"];
-        music.isfree = music.playUrl == "" ? 0 : 1;
-      } else {
-        throw ReqException("请求异常，请尝试打开网页版酷狗音乐解锁");
-      }
-    });
+    try {
+      String apiUrl = Util.stringFormat(ApiList.kugou_getMusicInfo,
+          [requestCallback, music.fileHash, music.albumId, timeStamp]);
+      Map<String, String> headers = {
+        "Accept":
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language":
+            "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+        "Cache-Control": "max-age=0",
+        "Connection": "keep-alive",
+        "Cookie":
+            "kg_mid=a1d421b9093150b003fbcd4de3ac3b15; Hm_lvt_aedee6983d4cfc62f509129360d6bb3d=1586421377,1586439179,1586509537,1586510990; kg_dfid=1aJS0t0U5yRr0qQNef02gPgO; Hm_lpvt_aedee6983d4cfc62f509129360d6bb3d=1586511002; kg_dfid_collect=d41d8cd98f00b204e9800998ecf8427e",
+        "Host": "wwwapi.kugou.com",
+        "If-Modified-Since": "Fri, 10 Apr 2020 09:55:57 GMT",
+        "TE": "Trailers",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0"
+      };
+      await http.get(apiUrl, headers: headers).then((response) {
+        RegExp reg = new RegExp(r"(?=" + requestCallback + "\()(.*)(?=\))");
+        var jsonStr = Util.onlyMatchOne(reg, response.body);
+        jsonStr = jsonStr.substring(1, jsonStr.length - 2);
+        var jsonMap = jsonDecode(jsonStr);
+        if (jsonMap["err_code"] == 0) {
+          music.coverUrl = jsonMap["data"]["img"];
+          music.playUrl = jsonMap["data"]["play_url"];
+          music.isfree = music.playUrl == "" ? 0 : 1;
+        } else {
+          throw ReqException("请求异常，请确定该网络是否可以正常使用酷狗音乐网页版播放歌曲");
+        }
+      });
+      return music;
+    } on ReqException catch (e) {
+      //电脑网页版接口获取不到歌曲信息，用手机版接口获取
+      return await getMusicInfoByMobileApi(music);
+    }
+  }
+
+  Future<Music> getMusicInfoByMobileApi(Music music) async {
+    try {
+      var playApiUrl =
+          Util.stringFormat(ApiList.kugou_mobile_musicinfo, [music.fileHash]);
+      Map<String, String> tempheaders = {
+        "Accept":
+            "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language":
+            "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
+        "Cache-Control": "max-age=0",
+        "Connection": "keep-alive",
+        "Cookie":
+            "kg_mid=a1d421b9093150b003fbcd4de3ac3b15; Hm_lvt_aedee6983d4cfc62f509129360d6bb3d=1586421377,1586439179,1586509537,1586510990; kg_dfid=1aJS0t0U5yRr0qQNef02gPgO; Hm_lpvt_aedee6983d4cfc62f509129360d6bb3d=1586511002; kg_dfid_collect=d41d8cd98f00b204e9800998ecf8427e",
+        "Host": "m3ws.kugou.com",
+        "TE": "Trailers",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent":
+            "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:75.0) Gecko/20100101 Firefox/75.0",
+        "Referer": "https://m3ws.kugou.com"
+      };
+      await http.get(playApiUrl, headers: tempheaders).then((response) {
+        var jsonMap = jsonDecode(response.body);
+        if (jsonMap["error"] == "需要付费") {
+          music.isfree = 0;
+          music.playUrl = "";
+        } else {
+          music.isfree = 1;
+          music.playUrl = jsonMap["backup_url"][0];
+        }
+      });
+      var imgApiUrl = Util.stringFormat(
+          ApiList.kugou_mobile_img, [music.fileHash, requestMobileImgCallback]);
+      await http.get(imgApiUrl).then((response) {
+        var jsonMap = jsonDecode(response.body);
+        if (jsonMap["status"] == 1) {
+          music.coverUrl = jsonMap["url"];
+        }
+      });
+      var lyricApiUrl =
+          Util.stringFormat(ApiList.kugou_mobile_lyric, [music.fileHash]);
+      var lyricStr = "";
+      await http.get(lyricApiUrl).then((response) {
+        lyricStr = utf8.decode(response.bodyBytes);
+      });
+      music.lyric = await getMusicLyrics(lyricStr);
+    } catch (e) {}
     return music;
   }
 
   @override
-  Future<List<Lyric>> getMusicLyrics(String lyricStr) async{
-    
+  Future<List<Lyric>> getMusicLyrics(String lyricStr) async {
+    List<Lyric> lyrics = List<Lyric>();
+    var lyricStrList = lyricStr.split('\n');
+    for(int i = 10;i<lyricStrList.length;i++){
+      if(lyricStrList[i].length < 10) continue;
+      var int_m = int.parse(lyricStrList[i].substring(1,3));
+      var int_s = int.parse(lyricStrList[i].substring(4,6));
+      var int_ms = int.parse(lyricStrList[i].substring(7,9));
+      var text = "";
+      if(lyricStrList[i].length >= 10){
+        text = lyricStrList[i].substring(10,lyricStrList[i].length);
+      }
+      if(text.isEmpty){
+        continue;
+      }
+      Lyric lyric = Lyric(
+        startMiSeconds: int_m*60000+int_s*1000+int_ms*10,
+        startTime: lyricStrList[i].substring(1,9),
+        text: text,
+      );
+      lyrics.add(lyric);
+    }
+    return lyrics;
   }
 }
